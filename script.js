6d3e214e15518acd6005d458193024d91bd99d43
leotapramudya946-sudo <leotapramudya946@gmail.com>
2026-05-08 23:00:56 +0700

    Update script.js
    2

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
+// script.js
+
 $(document).ready(function(){
+
+  // Animasi awal gift box
   $("#present").addClass("animated bounceInDown");
-  $("#present").hover(function(){
-      $("#present #top").addClass("animated bounce");
-  }, function(){
-      $("#present #top").removeClass("animated bounce");
-  });
+
+  // Hover animation
+  $("#present").hover(
+
+    function(){
+      $("#top").addClass("animated bounce");
+    },
+
+    function(){
+      $("#top").removeClass("animated bounce");
+    }
+
+  );
+
+  // Click animation
   $("#present").on("click", function(){
-    $("#present #top").removeClass("animated bounce");
-    $("#present #top").addClass("animated bounceOutUp");
+
+    $("#top")
+      .removeClass("animated bounce")
+      .addClass("animated bounceOutUp");
+
     $(this).unbind("click");
     $(this).unbind("mouseleave");
+
+    // Cat keluar
     setTimeout(function(){
-      $("#present #cat").css("top", "-160px");
+      $("#cat").css("top", "-160px");
     }, 1000);
-    $("#wish").children("div").each(function(index){
-      setTimeout(bounceIn.bind(null, $(this)), 2500+index*100)
-    });
+
+    // Animasi tulisan
+    $("#wish")
+      .children("div")
+      .each(function(index){
+
+        setTimeout(
+          bounceIn.bind(null, $(this)),
+          2500 + (index * 100)
+        );
+
+      });
+
   });
+
+  // Function animasi huruf
   function bounceIn(el){
+
     el.addClass("animated bounceInDown swing");
+
     setTimeout(function(){
-      el.removeClass("bounceInDown").addClass("swing")
+
+      el.removeClass("bounceInDown")
+        .addClass("swing");
+
     }, 1000);
+
   }
+
 });
